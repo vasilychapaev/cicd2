0b5bb2036b6dfb23b4ef3791f6c9ab1ae8abc44f
Alex R <alex.r@wmt.digital>
2025-03-08 12:00:26 +0100

    models, migrations - links, issues

---
+++ database/migrations/2025_03_08_104456_create_issues_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('issues', function (Blueprint $table) {
+            $table->id();
+            $table->string('subject');
+            $table->text('header_text');
+            $table->text('footer_text');
+            $table->timestamp('sent_at')->nullable();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('issues');
+    }
+};
